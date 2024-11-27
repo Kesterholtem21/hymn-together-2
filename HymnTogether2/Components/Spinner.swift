9a02ab0241017647ca3524da461d953987d3e9cc
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-27 15:25:10 -0500

    Use global loading. remove following.

---
+++ HymnTogether2/Components/Spinner.swift
@@ -0,0 +1,18 @@
+//
+//  Spinner.swift
+//  HymnTogether2
+//
+//  Created by Liam Grossman on 11/27/24.
+//
+
+import SwiftUI
+
+struct Spinner: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Spinner()
+}
---
+++ HymnTogether2/Views/AddHymnSingView.swift
@@ -0,0 +1,18 @@
+//
+//  AddHymnSingView.swift
+//  HymnTogether2
+//
+//  Created by Liam Grossman on 11/27/24.
+//
+
+import SwiftUI
+
+struct AddHymnSingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddHymnSingView()
+}
