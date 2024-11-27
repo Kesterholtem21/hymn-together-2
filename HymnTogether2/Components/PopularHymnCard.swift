73db8353e85fe2f095b466afdb0f73185641725b
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-27 16:01:08 -0500

    Add popular hymns

---
+++ HymnTogether2/Components/PopularHymnCard.swift
@@ -0,0 +1,18 @@
+//
+//  PopularHymnCard.swift
+//  HymnTogether2
+//
+//  Created by Liam Grossman on 11/27/24.
+//
+
+import SwiftUI
+
+struct PopularHymnCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PopularHymnCard()
+}
---
+++ HymnTogether2/Models/PopularHymnModel.swift
@@ -0,0 +1,8 @@
+//
+//  PopularHymnModel.swift
+//  HymnTogether2
+//
+//  Created by Liam Grossman on 11/27/24.
+//
+
+import Foundation
