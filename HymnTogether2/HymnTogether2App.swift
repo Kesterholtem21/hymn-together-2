b48ce59feee7df709dc0ffaf0b1bfcb3117ec15c
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-26 22:43:20 -0500

    Initial Commit

---
+++ HymnTogether2/HymnTogether2App.swift
@@ -0,0 +1,17 @@
+//
+//  HymnTogether2App.swift
+//  HymnTogether2
+//
+//  Created by Liam Grossman on 11/26/24.
+//
+
+import SwiftUI
+
+@main
+struct HymnTogether2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
