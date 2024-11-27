3e9664f92d305dd71d1a18de6d8951a3c4183bfc
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-27 10:25:51 -0500

    Fix onboarding. Add segmented control.

---
+++ HymnTogether2/Components/SelectedHymnOptions.swift
@@ -0,0 +1,18 @@
+//
+//  SelectedHymnOptions.swift
+//  HymnTogether2
+//
+//  Created by Liam Grossman on 11/27/24.
+//
+
+import SwiftUI
+
+struct SelectedHymnOptions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectedHymnOptions()
+}
