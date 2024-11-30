f92fabbe92711e201f305fa808541a0e1ef876f0
Liam Grossman <liam.j.grossman@gmail.com>
2024-11-30 13:21:57 -0500

    Format pdf

--- HymnTogether2/Components/HymnLyrics.swift
+++ HymnTogether2/Components/HymnLyrics.swift
@@ -9,21 +9,12 @@ import SwiftUI
 
 
 @MainActor
-func render(lyrics: [[String]], color: Color) -> String{
-    let renderer = ImageRenderer(content:
-            ForEach(0..<lyrics.count, id: \.self) { i in
-                VStack(alignment: .leading, spacing: 5) {
-                    ForEach(0..<lyrics[i].count, id: \.self) { j in
-                        let verse = lyrics[i][j]
-                        Text(verse)
-                    }
-                }
-            }
-        )
+func render(hymn: HymnModel) -> URL {
+    let renderer = ImageRenderer(content: PDFView(hymn: hymn))
     
     let url = URL.documentsDirectory.appending(path: "output.pdf")
     
-    renderer.render{ size, context in
+    renderer.render { size, context in
         var box = CGRect(x:0,y:0, width:size.width, height:size.height)
         
         guard let pdf = CGContext(url as CFURL, mediaBox: &box, nil) else{
@@ -36,16 +27,7 @@ func render(lyrics: [[String]], color: Color) -> String{
         pdf.closePDF()
     }
     
-    let urlString = url.absoluteString
-    return urlString
-    
-
-    
-    
-    
-    
-
-
+    return url
 }
 
 
@@ -53,7 +35,7 @@ func render(lyrics: [[String]], color: Color) -> String{
 
 struct HymnLyrics: View {
     @State var presentLyrics: Bool = false
-    let lyrics: [[String]]
+    let hymn: HymnModel
     let color: Color
     
     var body: some View {
@@ -61,37 +43,18 @@ struct HymnLyrics: View {
                 ScrollView {
                 VStack(alignment: .leading, spacing: 15) {
                     Text("Lyrics").font(.title2).bold()
-                    ForEach(0..<lyrics.count, id: \.self) { i in
+                    ForEach(0..<hymn.lyrics.count, id: \.self) { i in
                         VStack(alignment: .leading, spacing: 5) {
-                            ForEach(0..<lyrics[i].count, id: \.self) { j in
-                                let verse = lyrics[i][j]
+                            ForEach(0..<hymn.lyrics[i].count, id: \.self) { j in
+                                let verse = hymn.lyrics[i][j]
                                 Text(verse)
                             }
                         }
                     }
-//                    Button {
-//                        // TODO: Print lyrics view
-//                        //ShareLink("Export PDF", item: render(lyrics: lyrics, color: color))
-//                        //WebView(url: render(lyrics: lyrics, color: color))
-//                        //render(lyrics: lyrics, color: color)
-//                        //presentLyrics = true
-//                        
-//                    } label: {
-//                        ZStack {
-//                            RoundedRectangle(cornerRadius: 10.0).fill(color)
-//                            HStack {
-//                                Image(systemName: "printer")
-//                                    .resizable()
-//                                    .aspectRatio(contentMode: .fit)
-//                                    .frame(width: 25.0, height: 25.0)
-//                                Text("Print Lyrics")
-//                            }.padding().foregroundStyle(.white)
-//                        }.fixedSize(horizontal: false, vertical: true)
-//                    }
-//                   .sheet(isPresented: $presentLyrics){
-//                      WebView(url: render(lyrics: lyrics, color: color))
-//                }
-                    ShareLink("Export PDF", item: render(lyrics: lyrics, color: color))
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10.0).fill(color)
+                        ShareLink("Export PDF", item: render(hymn: hymn)).padding().foregroundStyle(.white)
+                    }.fixedSize(horizontal: false, vertical: true)
                 }
                 Spacer()
             }
@@ -101,5 +64,5 @@ struct HymnLyrics: View {
 }
 
 #Preview {
-    HymnLyrics(lyrics: [[]], color: .blue)
+    HymnLyrics(hymn: HymnModel(), color: .blue)
 }
