33729c0ac2657567578bf849283c8adafbbac46d
Liam Grossman <liam.j.grossman@gmail.com>
2024-12-11 12:23:44 -0500

    Indicate submission

--- HymnTogether2/Views/AddHymnSingView.swift
+++ HymnTogether2/Views/AddHymnSingView.swift
@@ -16,27 +16,34 @@ struct AddHymnSingView: View {
     @State var location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
     @State var showSheet = false
     @State var date: Date = Date()
+    @State var alert = (false, "")
     @EnvironmentObject var hymnSingVM: HymnSingViewModel
     @EnvironmentObject var personVM: PersonViewModel
     
     var body: some View {
         VStack(alignment: .leading, spacing: 15) {
             VStack(alignment: .leading, spacing: 10) {
-                Text("Name").bold()
+                HStack(spacing: 5) {
+                    Text("Name").bold()
+                    Text("*").bold().foregroundStyle(.red)
+                }
                 ZStack {
                     RoundedRectangle(cornerRadius: 10.0).fill(.gray).opacity(0.1)
                     TextField("Name", text: $name).padding(.horizontal)
                 }.frame(height: 50.0)
             }
             VStack(alignment: .leading, spacing: 10) {
-                Text("Description").bold()
+                HStack(spacing: 5) {
+                    Text("Description").bold()
+                    Text("*").bold().foregroundStyle(.red)
+                }
                 ZStack {
                     RoundedRectangle(cornerRadius: 10.0).fill(.gray).opacity(0.1)
                     TextField("Description", text: $description).padding(.horizontal).lineLimit(3)
                 }.frame(height: 50.0)
             }
             VStack(alignment: .leading, spacing: 10) {
-                DatePicker("Date", selection: $date, displayedComponents: .date).bold()
+                DatePicker("Date", selection: $date, in: Date()..., displayedComponents: .date).bold()
             }
             Button{
                 showSheet = true
@@ -54,32 +61,49 @@ struct AddHymnSingView: View {
                 LocationPicker(instructions: "Pick a location", coordinates: $location)
             }
             Button {
-                let hymnSing = HymnSingModel(
-                    personId: personVM.person.id,
-                    name: name,
-                    description: description,
-                    longitude: location.longitude,
-                    latitude: location.latitude,
-                    date: date.timeIntervalSince1970,
-                    person: PersonMetaModel(
-                        name: personVM.person.name,
-                        avatar: personVM.person.avatar
+                if name.isEmpty || description.isEmpty {
+                    alert.0 = true
+                    alert.1 = "Complete required fields"
+                } else {
+                    let hymnSing = HymnSingModel(
+                        personId: personVM.person.id,
+                        name: name,
+                        description: description,
+                        longitude: location.longitude,
+                        latitude: location.latitude,
+                        date: date.timeIntervalSince1970,
+                        person: PersonMetaModel(
+                            name: personVM.person.name,
+                            avatar: personVM.person.avatar
+                        )
                     )
-                )
-                Task {
-                      let createdHymnSing = await hymnSingVM.postHymnSing(hymnSing: hymnSing)
-                      await MainActor.run {
-                          personVM.mutateHymnSings(hymnSing:    createdHymnSing)
-                      }
+                    Task {
+                          let createdHymnSing = await hymnSingVM.postHymnSing(hymnSing: hymnSing)
+                          await MainActor.run {
+                              personVM.mutateHymnSings(hymnSing:    createdHymnSing)
+                          }
+                    }
+                    reset()
+                    alert.1 = "Hymn sing added"
+                    alert.0 = true
                 }
             } label: {
                 ZStack {
                     RoundedRectangle(cornerRadius: 10.0).fill(.blue)
                     Text("Add Hymn Sing").foregroundStyle(.white).padding(.horizontal)
                 }.frame(height: 50.0)
+            }.alert(isPresented: $alert.0) {
+                Alert(title: Text(alert.1))
             }
             Spacer()
         }.navigationTitle("Add Hymn Sing").padding(.horizontal)
+    }
+    
+    private func reset() {
+        self.name = ""
+        self.description = ""
+        self.date = Date()
+        self.location = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
     }
 }
 
